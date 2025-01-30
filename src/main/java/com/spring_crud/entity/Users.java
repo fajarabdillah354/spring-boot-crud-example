e871fd0856e33864931eba728b5d862550e3b4bc
fajar abdillah <fajardillah25@gmail.com>
2025-01-30 10:11:01 +0700

    replace with lambda for getter setter

--- src/main/java/com/spring_crud/entity/Users.java
+++ src/main/java/com/spring_crud/entity/Users.java
@@ -3,7 +3,10 @@ package com.spring_crud.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+
+@Data
 @Entity
 @Table(
         name = "users",
@@ -34,35 +37,4 @@ public class Users {
     private String password;
 
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
