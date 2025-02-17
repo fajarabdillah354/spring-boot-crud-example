94731eda5e0a41974ff534634fdf21e93bd84a87
fajar abdillah <fajardillah25@gmail.com>
2025-02-17 22:16:42 +0700

    Update

--- src/main/resources/db/migration/V1__01_CREATE_PRODUCT.sql
+++ src/main/resources/db/migration/V1__01_CREATE_PRODUCT.sql
@@ -1,5 +1,5 @@
 CREATE TABLE products(
-    product_id VARCHAR(25),
+    product_id VARCHAR(36),
     product_name VARCHAR(255) NOT NULL,
     product_description VARCHAR(255) NOT NULL,
     product_quantity INT DEFAULT 0,
