d8a67999b779c90c4af5ab5013942a3810fa45c4
fajar abdillah <fajardillah25@gmail.com>
2025-02-18 09:21:38 +0700

    Update remove getter setter and replace lombok

--- src/main/java/com/spring_crud/models/entity/ProductEntity.java
+++ src/main/java/com/spring_crud/models/entity/ProductEntity.java
@@ -2,6 +2,8 @@ package com.spring_crud.models.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -10,6 +12,8 @@ import java.util.Date;
 
 @Entity
 @Table(name = "products")
+@Data
+@NoArgsConstructor
 public class ProductEntity {
 
 
@@ -38,59 +42,5 @@ public class ProductEntity {
     @Column(name = "product_updated")
     private Date updatedAt;
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Integer getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(Integer productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public Integer getProductQuantity() {
-        return productQuantity;
-    }
-
-    public void setProductQuantity(Integer productQuantity) {
-        this.productQuantity = productQuantity;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
